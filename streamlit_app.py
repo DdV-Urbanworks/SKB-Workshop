6455cf52248d4c94d3afeab2ff16ed92e07d9171
DdV-Urbanworks <diederik.de.vries@urbanworks.se>
2025-05-21 20:39:28 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,47 +32,38 @@ with st.sidebar:
     Fpolitik = st.selectbox('Politisk riktning', pd.Series(range(11)))
     FTomträtt = st.selectbox('Tomträtt', pd.Series(range(11)))
     FDirektanvisar = st.selectbox('Direktanvisar', pd.Series(range(11)))
-    st.write('''
-        - **Politisk riktning**: 10 = vänster, 0 = höger
-        - **Tomträtt**: 10 = ja, 00 = nej
-        - **Direktanvisar**: 10 = ja, 0 = nej
-    ''')
     
-Fpolitik = 7
-FTomträtt = 3
-FDirektanvisar = 5
 
 gdf['poäng']=gdf['Betyg - Politik']*Fpolitik + gdf['Betyg - Direktanvisningar']*FDirektanvisar + gdf['Betyg - tomträtt']*FTomträtt
 gdf_sorted = gdf.sort_values(by='poäng', ascending=False)
 #gdf_sorted['normalized'] = gdf_sorted['total'] / gdf_sorted['total'].max()
 #gdf_sorted['poäng'] = (gdf_sorted['normalized'] * 100).round(1)
 
-print(gdf_sorted)
 
 
-"""
+
 #######################
 # Plots
 
 # Heatmap
-def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
-    heatmap = alt.Chart(input_df).mark_rect().encode(
-            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-            color=alt.Color(f'max({input_color}):Q',
-                             legend=None,
-                             scale=alt.Scale(scheme=input_color_theme)),
-            stroke=alt.value('black'),
-            strokeWidth=alt.value(0.25),
-        ).properties(width=900
-        ).configure_axis(
-        labelFontSize=12,
-        titleFontSize=12
-        ) 
+#def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
+#    heatmap = alt.Chart(input_df).mark_rect().encode(
+#            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+#            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+#            color=alt.Color(f'max({input_color}):Q',
+#                             legend=None,
+#                             scale=alt.Scale(scheme=input_color_theme)),
+#            stroke=alt.value('black'),
+#            strokeWidth=alt.value(0.25),
+#        ).properties(width=900
+#        ).configure_axis(
+#        labelFontSize=12,
+#        titleFontSize=12
+#        ) 
     # height=300
-    return heatmap
-"""
-print('check 0')
+#    return heatmap
+
+
 def make_map(gdf):
     color_scale = [
         (0, "rgba(255, 255, 255, 0.8)"),   
@@ -95,9 +86,8 @@ def make_map(gdf):
 FIG = make_map(gdf_sorted)
 FIG.show()
 
-#######################
-# Dashboard Main Panel
-col = st.columns((4.5, 2), gap='medium')
+
+col = st.columns((3, 2), gap='large')
 
 with col[0]:
     st.markdown('#### Var ska vi investera?')
@@ -105,15 +95,12 @@ with col[0]:
     Map = make_map(gdf)
     st.plotly_chart(Map, use_container_width=True)
     
-    """
-    heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
-    st.altair_chart(heatmap, use_container_width=True)
-    """
+
 
 
 
 with col[1]:
-    st.markdown('#### Min bästa kommun')
+    st.markdown('#### Kommuner med högst poäng')
 
 st.dataframe(gdf.drop(columns='geometry'))
 
